be13cba3fc0b33d3053bc733b4c80bfa50a4c848
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-04-07 20:13:03 +0600

    set main routing and toast

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 import App from './App.jsx'
 import './index.css'
 import router from './routes/router.jsx'
@@ -8,5 +9,6 @@ import router from './routes/router.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    {/* <ToastContainer autoClose={1000}></ToastContainer> */}
   </React.StrictMode>,
 )
